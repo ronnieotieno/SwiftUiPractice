75e2c4e384bf70bbdb4d91df2a47dd6f9543763f
Ronnie Otieno <ronnieotieno2@gmail.com>
2021-08-21 15:00:51 +0300

    Initial Commit

---
+++ SwitUiPrep/SwitUiPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitUiPrepApp.swift
+//  SwitUiPrep
+//
+//  Created by Ronnie Otieno on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwitUiPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
